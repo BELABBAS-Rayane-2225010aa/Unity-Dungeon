58eac4ab3048a15f28ebae4678aae8b6ddaa2c4c
Crespin Alexandre <126071849+CRESPIN-Alexandre-2225022aa@users.noreply.github.com>
2024-10-19 12:50:35 +0200

    feat : modification de la génération du donjon
    Utilisation d'un nouvelle algorithme pour la génération du donjon : Prism
    Merci à Copilot pour la grande aide en recherche et implémentation même si cela a été très dur
    Ref : #9

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,8 +8,20 @@ public class DungeonGenerator : MonoBehaviour
 {
     public class Cell 
     {
-        public bool visited = false;
         public bool[] status = new bool[4];
+        public bool visited = false;
+    }
+
+    public struct Edge
+    {
+        public Vector2Int from;
+        public Vector2Int to;
+
+        public Edge(Vector2Int from, Vector2Int to)
+        {
+            this.from = from;
+            this.to = to;
+        }
     }
 
     [System.Serializable]
@@ -17,15 +30,13 @@ public class DungeonGenerator : MonoBehaviour
         public GameObject room;
         public Vector2Int minPosition;
         public Vector2Int maxPosition;
-
         public bool[] EntranceExistence = new bool[4];
     }
 
     public Vector2Int size;
     public Vector3Int startVectorPos;
-    public Vector2Int startPos = new Vector2Int(0,0);
+    public Vector2Int startPos = new Vector2Int(0, 0);
     public Vector2 offset;
-    public float noise = 0.5f;
     public Rule[] rooms;
 
     List<List<Cell>> board;
@@ -34,205 +45,196 @@ public class DungeonGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        To generate the dungeon we uses a homebrew random dungeon generator
-        First we generate a grid of Cell of the size of the dungeon
-        Then we generate a dungeon that fullfil the grid with the full room
-        Then we choose the last room
-        Finnaly we generate the doors and a path to the last room
-        */
         GridGenerator();
         GenerateDungeon();
+        MarkLastCell();
     }
 
-    void GridGenerator(){
+    void GridGenerator() {
         board = new List<List<Cell>>();
         roomGrid = new RoomBehavior[size.x, size.y];
 
-        for (int i = 0; i < size.x; i++)
-        {
-            for (int j = 0; j < size.y; j++)
-            {
-                board.Add(new Cell());
+        for (int i = 0; i < size.x; i++) {
+            board.Add(new List<Cell>());
+
+            for (int j = 0; j < size.y; j++) {
+                Cell newCell = new Cell();
+                board[i].Add(newCell);
             }
         }
     }
 
-    void GenerateDungeon(){
+    void GenerateDungeon() {
         // Generation of the blank dungeon
-        // Generation of the last Cell
-        // This Cell can be where ever it want but cannot be on the first position
-        GameObject lastCell = new();
-        int boardPos = Random.Range(1,board.Count - 1);
         for (int i = 0; i < size.x; i++)
         {
             for (int j = 0; j < size.y; j++)
             {
-                Cell currentCell = board[i + j * size.x];
+                Cell currentCell = board[i][j];
                 var roomPrefab = rooms[0].room;
-                RoomBehavior newRoom;
-                if (i + j * size.x == boardPos)
-                {
-                    lastCell = Instantiate(roomPrefab, new Vector3(i * offset.x + startVectorPos.x, startVectorPos.y, -j * offset.y + startVectorPos.z), roomPrefab.transform.rotation, transform);
-                    newRoom = lastCell.GetComponent<RoomBehavior>();
-                    newRoom.UpdateRoom(currentCell.status);
-                    newRoom.UpdateLastCell(true);
-                }
-                else
-                {
-                    newRoom = Instantiate(roomPrefab, new Vector3(i * offset.x + startVectorPos.x, startVectorPos.y, -j * offset.y + startVectorPos.z), roomPrefab.transform.rotation, transform).GetComponent<RoomBehavior>();
-                    newRoom.UpdateRoom(currentCell.status);
-                }
-
+                RoomBehavior newRoom = Instantiate(roomPrefab, 
+                    new Vector3(j * offset.x + startVectorPos.x, startVectorPos.y, -i * offset.y + startVectorPos.z), 
+                    roomPrefab.transform.rotation, transform).GetComponent<RoomBehavior>();
+                newRoom.name = "Room" + i + "|" + j;
                 roomGrid[i, j] = newRoom;
             }
         }
-        if (lastCell)
+
+        // Call Prim's algorithm to generate the maze
+        GenerateMaze();
+
+        // Replace rooms based on EntranceExistence
+        ReplaceRooms();
+    }
+
+    void GenerateMaze()
+    {
+        List<Edge> edges = new List<Edge>();
+        HashSet<Vector2Int> visitedCells = new HashSet<Vector2Int>();
+
+        Vector2Int startCell = new Vector2Int(Random.Range(0, size.x), Random.Range(0, size.y));
+        visitedCells.Add(startCell);
+        AddEdges(startCell, edges);
+
+        while (edges.Count > 0)
         {
-            PathGenerator(lastCell);
+            Edge edge = edges[Random.Range(0, edges.Count)];
+            edges.Remove(edge);
+
+            if (!visitedCells.Contains(edge.to))
+            {
+                visitedCells.Add(edge.to);
+                OpenDoor(edge.from, edge.to);
+                AddEdges(edge.to, edges);
+            }
+        }
+
+        // Update the visual representation of the dungeon
+        UpdateDoors();
+    }
+
+    void AddEdges(Vector2Int cell, List<Edge> edges)
+    {
+        int x = cell.x;
+        int y = cell.y;
+
+        if (x > 0) edges.Add(new Edge(cell, new Vector2Int(x - 1, y))); // Left
+        if (x < size.x - 1) edges.Add(new Edge(cell, new Vector2Int(x + 1, y))); // Right
+        if (y > 0) edges.Add(new Edge(cell, new Vector2Int(x, y - 1))); // Down
+        if (y < size.y - 1) edges.Add(new Edge(cell, new Vector2Int(x, y + 1))); // Up
+    }
+
+    void OpenDoor(Vector2Int from, Vector2Int to)
+    {
+        if (from.x == to.x)
+        {
+            if (from.y > to.y)
+            {
+                board[from.x][from.y].status[3] = true; // Open door to the left
+                board[to.x][to.y].status[2] = true; // Open door to the right
+            }
+            else
+            {
+                board[from.x][from.y].status[2] = true; // Open door to the right
+                board[to.x][to.y].status[3] = true; // Open door to the left
+            }
+        }
+        else
+        {
+            if (from.x > to.x)
+            {
+                board[from.x][from.y].status[0] = true; // Open door to the top
+                board[to.x][to.y].status[1] = true; // Open door to the bottom
+            }
+            else
+            {
+                board[from.x][from.y].status[1] = true; // Open door to the bottom
+                board[to.x][to.y].status[0] = true; // Open door to the top
+            }
         }
     }
 
-    void PathGenerator(GameObject lastCell){
-        int totalCells = size.x * size.y;
-        Vector2Int currentPos = startPos;
-        Vector2Int endPos = new Vector2Int((int)(lastCell.transform.position.x / offset.x), -(int)(lastCell.transform.position.z / offset.y));
-
-        List<Vector2Int> unvisitedCells = new List<Vector2Int>();
+    void UpdateDoors() {
         for (int i = 0; i < size.x; i++)
         {
             for (int j = 0; j < size.y; j++)
             {
-                unvisitedCells.Add(new Vector2Int(i, j));
+                Cell currentCell = board[i][j];
+                UpdateRoom(currentCell, i, j);
             }
         }
-        unvisitedCells.Remove(currentPos);
+    }
 
+    void UpdateRoom(Cell cell, int i, int j){
+        // Conversion du tableau de booléens en chaîne de caractères
+        string statusValues = string.Join(", ", cell.status.Select(b => b.ToString()).ToArray());
 
-        List<Vector2Int> path = new List<Vector2Int>();
-        path.Add(currentPos);
+        // Affichage en une seule fois avec la position de la currentRoom
+        Debug.Log("Room : (" + i + ", " + j + ") : " + statusValues);
 
-        while (unvisitedCells.Count > 0)
+        // Mettre à jour le visuel de la salle
+        RoomBehavior room = roomGrid[i, j];
+        room.UpdateRoom(cell.status);
+    }
+
+    void ReplaceRooms() {
+        for (int i = 0; i < size.x; i++)
         {
-            List<Vector2Int> neighbours = GetUnvisitedNeighbours(currentPos, unvisitedCells);
-
-            if (neighbours.Count > 0)
+            for (int j = 0; j < size.y; j++)
             {
-                Vector2Int nextPos;
-                if (Random.value < noise)
+                Cell currentCell = board[i][j];
+                GameObject roomPrefab = SelectRoomPrefab(currentCell.status);
+                Vector3 position = new Vector3(j * offset.x + startVectorPos.x, startVectorPos.y, -i * offset.y + startVectorPos.z);
+                Quaternion rotation = roomPrefab.transform.rotation;
+
+                // Détruire l'ancienne salle
+                Destroy(roomGrid[i, j].gameObject);
+
+                // Instancier la nouvelle salle
+                RoomBehavior newRoom = Instantiate(roomPrefab, position, rotation, transform).GetComponent<RoomBehavior>();
+                newRoom.name = "Room" + i + "|" + j;
+                roomGrid[i, j] = newRoom;
+
+                // Mettre à jour le visuel de la salle
+                newRoom.UpdateRoom(currentCell.status);
+            }
+        }
+    }
+
+    GameObject SelectRoomPrefab(bool[] status)
+    {
+        List<Rule> validRooms = new List<Rule>();
+
+        foreach (var room in rooms)
+        {
+            bool isValid = true;
+            for (int i = 0; i < 4; i++)
+            {
+                if (room.EntranceExistence[i] != status[i])
                 {
-                    nextPos = neighbours[Random.Range(0,neighbours.Count)];
+                    isValid = false;
+                    break;
                 }
-                else
-                {
-                    nextPos = GetClosestNeighbourToTarget(neighbours, endPos);
-                }
-
-                MarkPathOnBoard(currentPos, nextPos);
-
-                currentPos = nextPos;
-                path.Add(currentPos);
-                unvisitedCells.Remove(currentPos);
             }
-            else
+            if (isValid)
             {
-                currentPos = path[path.Count - 2];
-                path.RemoveAt(path.Count - 1);
+                validRooms.Add(room);
             }
         }
+
+        if (validRooms.Count == 0)
+        {
+            return rooms[0].room; // Default room if no match found
+        }
+
+        return validRooms[Random.Range(0, validRooms.Count)].room;
     }
 
-    List<Vector2Int> GetUnvisitedNeighbours(Vector2Int currentPos, List<Vector2Int> unvisitedCells)
+    void MarkLastCell()
     {
-        List<Vector2Int> neighbours = new List<Vector2Int>();
+        // Mark the last room in the grid as the last cell
+        RoomBehavior lastRoom = roomGrid[size.x - 1, size.y - 1];
 
-        if (currentPos.x > 0 && unvisitedCells.Contains(new Vector2Int(currentPos.x - 1, currentPos.y)))
-            neighbours.Add(new Vector2Int(currentPos.x - 1, currentPos.y));  // Gauche
-        if (currentPos.x < size.x - 1 && unvisitedCells.Contains(new Vector2Int(currentPos.x + 1, currentPos.y)))
-            neighbours.Add(new Vector2Int(currentPos.x + 1, currentPos.y));  // Droite
-        if (currentPos.y > 0 && unvisitedCells.Contains(new Vector2Int(currentPos.x, currentPos.y - 1)))
-            neighbours.Add(new Vector2Int(currentPos.x, currentPos.y - 1));  // Bas
-        if (currentPos.y < size.y - 1 && unvisitedCells.Contains(new Vector2Int(currentPos.x, currentPos.y + 1)))
-            neighbours.Add(new Vector2Int(currentPos.x, currentPos.y + 1));  // Haut
-
-        return neighbours;
-    }
-
-
-    Vector2Int GetClosestNeighbourToTarget(List<Vector2Int> neighbours, Vector2Int target)
-    {
-        Vector2Int closest = neighbours[0];
-        float minDist = Vector2Int.Distance(neighbours[0], target);
-
-        foreach (var neighbour in neighbours)
-        {
-            float dist = Vector2Int.Distance(neighbour, target);
-            if (dist < minDist)
-            {
-                minDist = dist;
-                closest = neighbour;
-            }
-        }
-        return closest;
-    }
-
-    void MarkPathOnBoard(Vector2Int from, Vector2Int to)
-    {
-        // Vérifie d'abord que les deux cellules sont valides
-        if (!IsValidCell(from) || !IsValidCell(to)) return;
-
-        Cell currentCell = board[from.x + from.y * size.x];
-        Cell nextCell = board[to.x + to.y * size.x];
-
-        // Gère les déplacements horizontaux
-        if (from.x < to.x)  // Aller à droite
-        {
-            currentCell.status[2] = true;  // Porte droite ouverte
-            nextCell.status[3] = true;     // Porte gauche ouverte
-        }
-        else if (from.x > to.x)  // Aller à gauche
-        {
-            currentCell.status[3] = true;  // Porte gauche ouverte
-            nextCell.status[2] = true;     // Porte droite ouverte
-        }
-        // Gère les déplacements verticaux
-        else if (from.y < to.y)  // Aller en haut
-        {
-            currentCell.status[0] = true;  // Porte haut ouverte
-            nextCell.status[1] = true;     // Porte bas ouverte
-        }
-        else if (from.y > to.y)  // Aller en bas
-        {
-            currentCell.status[1] = true;  // Porte bas ouverte
-            nextCell.status[0] = true;     // Porte haut ouverte
-        }
-
-        // Met à jour les objets visuels dans la scène (les salles)
-        var roomFrom = GetRoomAtPosition(from);
-        var roomTo = GetRoomAtPosition(to);
-
-        if (roomFrom != null)
-        {
-            roomFrom.UpdateRoom(currentCell.status);
-        }
-
-        if (roomTo != null)
-        {
-            roomTo.UpdateRoom(nextCell.status);
-        }
-    }
-
-    // Ajout d'une fonction utilitaire pour vérifier si une cellule est valide
-    bool IsValidCell(Vector2Int position)
-    {
-        return position.x >= 0 && position.x < size.x && position.y >= 0 && position.y < size.y;
-    }
-
-    RoomBehavior GetRoomAtPosition(Vector2Int position){
-        if (position.x >= 0 && position.x < size.x && position.y >= 0 && position.y < size.y)
-        {
-            return roomGrid[position.x,position.y];
-        }
-        return null;
+        lastRoom.UpdateLastCell(true);
     }
 }
